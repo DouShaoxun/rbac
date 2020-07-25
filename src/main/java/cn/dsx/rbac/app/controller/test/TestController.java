f882420c6b2889b2d0a8178680648022ba5ef472
DouShaoxun <dsx1046811165@gmail.com>
2020-07-26 01:07:31 +0800

    test

--- src/main/java/cn/dsx/rbac/app/controller/test/TestController.java
+++ src/main/java/cn/dsx/rbac/app/controller/test/TestController.java
@@ -3,10 +3,15 @@ package cn.dsx.rbac.app.controller.test;
 import cn.dsx.rbac.common.result.Result;
 import cn.dsx.rbac.common.utils.ResultUtils;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.wf.jwtp.annotation.Ignore;
 import org.wf.jwtp.annotation.RequiresPermissions;
+import org.wf.jwtp.annotation.RequiresRoles;
+import org.wf.jwtp.provider.Token;
+import org.wf.jwtp.util.SubjectUtil;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @Classname: TestController
@@ -15,13 +20,27 @@ import org.wf.jwtp.annotation.RequiresPermissions;
  */
 @RestController
 @RequestMapping("/app/test")
+@RequiresRoles("admin")
 public class TestController {
 
-    @RequiresPermissions("system")
-    @PostMapping("/")
-    public Result test1(@RequestBody String name) {
 
+    @RequiresPermissions("system")
+    @PostMapping("/1")
+    public Result test1(String name,HttpServletRequest request) {
+        Token token = SubjectUtil.getToken(request);
         return ResultUtils.ok().data("name", name);
 
     }
+
+
+
+    @Ignore // 忽略 权限认证
+    @PostMapping("/2")
+    public Result test2(String name, HttpServletRequest request)  {
+        Token token = SubjectUtil.getToken(request);// @Ignore token 为null
+        System.out.println(name);
+        return ResultUtils.ok().data("name", name);
+    }
+
+
 }
