2d35eb101f18385231e1973d784af14e954a68c9
DouShaoxun <dsx1046811165@gmail.com>
2020-08-01 00:06:16 +0800

    mete

---
+++ src/main/java/cn/dsx/rbac/app/bean/vo/Meta.java
@@ -0,0 +1,14 @@
+package cn.dsx.rbac.app.bean.vo;
+
+import lombok.Data;
+
+/**
+ * @Classname: Meta
+ * @Author: Dsx
+ * @Date: 2020/07/26/17:50
+ */
+@Data
+public class Meta {
+    private String icon;
+    private String title;
+}
